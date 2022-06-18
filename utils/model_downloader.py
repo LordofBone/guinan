40b496bfe287aac7834d6c6c58d5aeac168c0c91
314Reactor <34923578+LordofBone@users.noreply.github.com>
2022-06-18 21:09:36 +0100

    Updated to remove some debugging entries

--- utils/model_downloader.py
+++ utils/model_downloader.py
@@ -7,13 +7,10 @@ from utils.load_bar_non_iterable import progress_bar
               expected_time=180)
 def download_models():
     model_url = "https://github.com/mozilla/DeepSpeech/releases/download/v0.9.3/deepspeech-0.9.3-models.pbmm"
-    response_model = wget.download(model_url, f"{model_path}/deepspeech-0.9.3-models.pbmm")
+    wget.download(model_url, f"{model_path}/deepspeech-0.9.3-models.pbmm")
 
     scorer_url = "https://github.com/mozilla/DeepSpeech/releases/download/v0.9.3/deepspeech-0.9.3-models.scorer"
-    response_scorer = wget.download(scorer_url, f"{model_path}/deepspeech-0.9.3-models.scorer")
-
-    print(response_model)
-    print(response_scorer)
+    wget.download(scorer_url, f"{model_path}/deepspeech-0.9.3-models.scorer")
 
 
 if __name__ == "__main__":
